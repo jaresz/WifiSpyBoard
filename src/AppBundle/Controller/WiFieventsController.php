9ca88ac80cdfe24f826df3eea58164a78c23dc0a
Jakub <jaresz.git@listy.internetowe.pl>
2017-10-23 01:30:57 +0200

    WiFi sessions with host column

--- src/AppBundle/Controller/WiFieventsController.php
+++ src/AppBundle/Controller/WiFieventsController.php
@@ -58,7 +58,7 @@ class WiFieventsController extends Controller
     /**
      * Lists all systemevent entities.
      *
-     * @Route("/mac/", name="wifi_events_mac_index")
+     * @Route("/mac/", name="wifi_sessions")
      * @Method("GET")
      */
     public function indexMacAction()
@@ -74,53 +74,56 @@ class WiFieventsController extends Controller
         $clientTools = new ClientTools($em);
         
         $fromPoinInTime = new \DateTime();
-        $fromPoinInTime->modify('-2 days');
+        $fromPoinInTime->modify('-7 days');
     
         $conn = $this->getDoctrine()->getConnection();
-        $conn->fetchAll("SELECT SystemEvents.ID, SystemEvents.ReceivedAt,
-                eventConnected.mac as macC,
-                eventDisconnected.mac as macD,
-                CONCAT(COALESCE(eventConnected.mac,'') , COALESCE(eventDisconnected.mac, '')) as mac
+        
+        $lastConns = $conn->fetchAll("SELECT SystemEvents.FromHost, eventConnected.mac as mac,
+                MAX(SystemEvents.ReceivedAt) AS lastTime
                 FROM SystemEvents
                 LEFT JOIN eventConnected ON eventConnected.system_event_connected_id = SystemEvents.ID
-                LEFT JOIN eventDisconnected ON eventDisconnected.system_event_disconnected_id = SystemEvents.ID
-                WHERE (SystemEvents.message LIKE '% connected' OR SystemEvents.message LIKE '% disconnected%')
-                AND SystemEvents.ReceivedAt>= ?
+                WHERE (SystemEvents.message LIKE '% connected' )
+                AND SystemEvents.syslogtag = 'WiFi:'
+                AND SystemEvents.ReceivedAt >= ?
+                GROUP BY SystemEvents.FromHost, eventConnected.mac
                 HAVING mac IS NOT NULL AND mac!=''
-                ORDER BY mac, macC IS NULL, SystemEvents.ID", [$fromPoinInTime->format('Y-m-d H:i:s')]
-                );
-        
-        $q1Builder = $em->createQueryBuilder()
-        ->select([
-            'Systemevents',
-            'EventConnected',
-            'EventDisconnected'
-        ])
-        ->from('AppBundle\Entity\Systemevents', 'Systemevents')
-        ->leftJoin('Systemevents.eventConnected', 'EventConnected')
-        ->leftJoin('Systemevents.eventDisconnected', 'EventDisconnected')
-        ->where("Systemevents.syslogtag=:syslogtag")
-        ->setParameter('syslogtag', 'WiFi:')
-        ->andWhere("EventConnected.mac IS NOT NULL")
-        //->andWhere("Systemevents.id IN (32508, 32513)")
-        ->andWhere("(Systemevents.message LIKE '% connected' OR Systemevents.message LIKE '% disconnected%')")
-        ->andWhere("Systemevents.devicereportedtime > :fromPoinInTime")
-        ->setParameter('fromPoinInTime', $fromPoinInTime)
-        ->addOrderBy('Systemevents.id', 'DESC')
-        ;
-    
-        $wifiConnections = $q1Builder->getQuery()->getResult();
-    
-        foreach ($wifiConnections as $wifiConnection) {
-            /**
-             * @var Systemevents $systemevent
-             */
-            $message = $clientTools->replaceMacsByNames($wifiConnection->getMessage(), true);
-            $wifiConnection->setMessage($message);
+                ORDER BY lastTime DESC, mac, SystemEvents.FromHost", [$fromPoinInTime->format('Y-m-d H:i:s')]
+            );
+       
+        $sessionsInfo = [];
+        foreach($lastConns as $lastConn) {            
+           //dump($lastConn);
+           $sessionInfo = [             
+             'clientInfo' => $lastConn['mac'],
+             'FromHost' => $lastConn['FromHost'],
+             'mac' => $lastConn['mac'],
+             'connectedOn' => $lastConn['lastTime'],
+             'disconnectedOn' => null
+           ];
+           
+           $lastDisconn = $conn->fetchAssoc("SELECT SystemEvents.ID, SystemEvents.ReceivedAt,
+                eventDisconnected.mac as mac
+                FROM SystemEvents
+                LEFT JOIN eventDisconnected ON eventDisconnected.system_event_disconnected_id = SystemEvents.ID
+                WHERE (SystemEvents.message LIKE '% disconnected%')
+                AND SystemEvents.syslogtag = 'WiFi:'
+                AND SystemEvents.ReceivedAt>= ?
+                AND eventDisconnected.mac = ?
+                AND SystemEvents.FromHost = ?
+                ", [$lastConn['lastTime'], $lastConn['mac'], $lastConn['FromHost']]);
+           //dump($lastDisconn);
+           if ($lastDisconn) 
+               $sessionInfo['disconnectedOn'] =  $lastDisconn['ReceivedAt'];
+           
+           $sessionInfo['clientInfo'] = $clientTools->replaceMacsByNames($lastConn['mac'], true);
+               
+           $sessionsInfo[] = $sessionInfo;
         }
-        // ->setMaxResults(5)
-        return $this->render('systemevents/mac_index.html.twig', array(
-            'systemevents' => $wifiConnections
+        
+        
+        
+        return $this->render('systemevents/sessions.html.twig', array(
+            'sessionsInfo' => $sessionsInfo
         ));
     }
     
